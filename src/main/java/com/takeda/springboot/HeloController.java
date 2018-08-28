b90de19b4cece70734919445336f2daa95b63256
武田鴻太 <xxk0ta0605@gmail.com>
2018-08-28 20:34:40 +0900

    HeloControllerの作成

---
+++ src/main/java/com/takeda/springboot/HeloController.java
@@ -0,0 +1,5 @@
+package com.takeda.springboot;
+
+public class HeloController {
+
+}
--- src/main/java/com/takeda/springboot/MyBootAppApplication.java
+++ src/main/java/com/takeda/springboot/MyBootAppApplication.java
@@ -3,7 +3,7 @@ package com.takeda.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//このクラスはspringbootのアプリですよー
 public class MyBootAppApplication {
 
 	public static void main(String[] args) {
