3637da5653ab64bcf7aca62480eaa372a8ed3a21
武田鴻太 <xxk0ta0605@gmail.com>
2018-08-28 20:45:26 +0900

    Hello World

--- src/main/java/com/takeda/springboot/HeloController.java
+++ src/main/java/com/takeda/springboot/HeloController.java
@@ -1,5 +1,13 @@
 package com.takeda.springboot;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HeloController {
 
+	@RequestMapping("/")
+	public String index() {
+		return "Hello World!!";
+	}
 }
