f057ff3f3667a9a03fcf58861d8aa1aa7d0bbd28
武田鴻太 <xxk0ta0605@gmail.com>
2018-08-30 21:34:40 +0900

    Model→ModelAndViewに変更して動作を確認

--- src/main/java/com/takeda/springboot/HeloController.java
+++ src/main/java/com/takeda/springboot/HeloController.java
@@ -1,21 +1,26 @@
 package com.takeda.springboot;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller//このクラスはControllerですよ
 public class HeloController {
 
-	@RequestMapping("/{num}")//サーバーが受け取るアドレス
-	public String index(@PathVariable int num, Model model) {
-		//テンプレート名。pom.xmlでspring-boot-starter-thymeleafを指定してるから使える
-		int res = 0;
-		for (int i = 1; i <= num; i++) {
-			res += i;
-		}
-		model.addAttribute("msg", "total: " + res);
-		return "index";
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg","お名前を書いて送信してください。");
+		return mav;
+	}
+	
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView send(@RequestParam("text1")String str, ModelAndView mav) {
+		mav.addObject("msg", "こんにちは, " + str + "さん" );
+		mav.addObject("value", str);
+		mav.setViewName("index");
+		return mav;
 	}
 }
