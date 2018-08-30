19bc224b442c24c5885178554c683c96e41810ed
武田鴻太 <xxk0ta0605@gmail.com>
2018-08-30 20:41:04 +0900

    値の引き渡しを可能に

--- src/main/java/com/takeda/springboot/HeloController.java
+++ src/main/java/com/takeda/springboot/HeloController.java
@@ -1,13 +1,21 @@
 package com.takeda.springboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller//このクラスはControllerですよ
 public class HeloController {
 
-	@RequestMapping("/")//サーバーが受け取るアドレス
-	public String index() {
+	@RequestMapping("/{num}")//サーバーが受け取るアドレス
+	public String index(@PathVariable int num, Model model) {
+		//テンプレート名。pom.xmlでspring-boot-starter-thymeleafを指定してるから使える
+		int res = 0;
+		for (int i = 1; i <= num; i++) {
+			res += i;
+		}
+		model.addAttribute("msg", "total: " + res);
 		return "index";
 	}
 }
