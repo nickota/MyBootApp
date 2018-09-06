b39553af470edba61cc6489ff3b80f340d4f9381
武田鴻太 <xxk0ta0605@gmail.com>
2018-09-06 23:06:32 +0900

    テンプレートでswitch

--- src/main/java/com/takeda/springboot/HeloController.java
+++ src/main/java/com/takeda/springboot/HeloController.java
@@ -30,13 +30,13 @@ public class HeloController {
 		return "forward:/";
 	}
 	
-	@RequestMapping("/{id}")
-	public ModelAndView index(@PathVariable int id, ModelAndView mav) {
+	@RequestMapping("/{month}")
+	public ModelAndView index(@PathVariable int month, ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("id", id);
-		mav.addObject("check", id >= 0);
-		mav.addObject("trueVal", "POSITIVE!!");
-		mav.addObject("falseVal", "negative...");
+		int m = Math.abs(month) % 12;
+		m = m == 0 ? 12 : m;
+		mav.addObject("month",m);
+		mav.addObject("check", Math.floor(m / 3));
 		return mav;
 	}
 }
