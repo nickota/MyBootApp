76741b84e7d7d666e70f3b1446dd98d784d589a4
武田鴻太 <xxk0ta0605@gmail.com>
2018-09-06 22:13:53 +0900

    テンプレート上で条件分岐させる

--- src/main/java/com/takeda/springboot/HeloController.java
+++ src/main/java/com/takeda/springboot/HeloController.java
@@ -1,21 +1,22 @@
 package com.takeda.springboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller//このクラスはControllerですよ
 public class HeloController {
 
-	@RequestMapping("/")
-	public ModelAndView index(ModelAndView mav) {
-		mav.setViewName("index");
-		mav.addObject("msg","current data");
-		mav.addObject("msg2","message1<hr/>message2<br>message3");
-		DataObject obj = new DataObject(123, "hanako", "hanako@flower");
-		mav.addObject("object",obj);
-		return mav;
-	}
+//	@RequestMapping("/")
+//	public ModelAndView index(ModelAndView mav) {
+//		mav.setViewName("index");
+//		mav.addObject("msg","current data");
+//		mav.addObject("msg2","message1<hr/>message2<br>message3");
+//		DataObject obj = new DataObject(123, "hanako", "hanako@flower");
+//		mav.addObject("object",obj);
+//		return mav;
+//	}
 	
 	@RequestMapping(value="/other")
 	public String other() {
@@ -27,6 +28,16 @@ public class HeloController {
 	public String home() {
 		//「/」にforwardしてる
 		return "forward:/";
+	}
+	
+	@RequestMapping("/{id}")
+	public ModelAndView index(@PathVariable int id, ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("id", id);
+		mav.addObject("check", id % 2 == 0);
+		mav.addObject("trueVal", "Even number!");
+		mav.addObject("falseVal", "Odd number!");
+		return mav;
 	}
 }
 
