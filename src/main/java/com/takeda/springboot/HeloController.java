2c1caea740cf9f14eb24e73a603cd537383545ed
武田鴻太 <xxk0ta0605@gmail.com>
2018-09-06 21:29:22 +0900

    objectの受け渡し

--- src/main/java/com/takeda/springboot/HeloController.java
+++ src/main/java/com/takeda/springboot/HeloController.java
@@ -7,9 +7,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller//このクラスはControllerですよ
 public class HeloController {
 
-	@RequestMapping(value="/")
+	@RequestMapping("/")
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
+		mav.addObject("msg","current data");
+		DataObject obj = new DataObject(123, "hanako", "hanako@flower");
+		mav.addObject("object",obj);
 		return mav;
 	}
 	
@@ -24,4 +27,42 @@ public class HeloController {
 		//「/」にforwardしてる
 		return "forward:/";
 	}
+}
+
+class DataObject {
+	private int id;
+	private String name;
+	private String value;
+	
+	public DataObject(int id, String name, String value) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.value = value;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
 }
