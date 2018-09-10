4e87d34f5899ef3acccfc1f9d36e012e9f4d0349
武田鴻太 <xxk0ta0605@gmail.com>
2018-09-10 21:10:50 +0900

    インライン処理

--- src/main/java/com/takeda/springboot/HeloController.java
+++ src/main/java/com/takeda/springboot/HeloController.java
@@ -1,7 +1,6 @@
 package com.takeda.springboot;
 
 import java.util.ArrayList;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,45 +9,16 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller//このクラスはControllerですよ
 public class HeloController {
 
-	@RequestMapping("/{num}")
-	public ModelAndView index(@PathVariable int num, ModelAndView mav) {
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("num", num);
-		if (num >= 0) {
-			mav.addObject("check", "num >= data.size() ? 0 : num");
-		} else {
-			mav.addObject("check", "num <= data.size() * -1 ? 0 : num * -1");
-		}
-		
 		ArrayList<DataObject> data = new ArrayList<DataObject>();
 		data.add(new DataObject(0, "taro", "taro@yamada"));
-		data.add(new DataObject(1, "hanako", "hanako@flower"));
-		data.add(new DataObject(2, "sachiko", "sachiko@com"));
+		data.add(new DataObject(1, "haanko", "hanako@yamada"));
+		data.add(new DataObject(0, "sachiko", "sachiko@yamada"));
 		mav.addObject("data", data);
 		return mav;
 	}
-	
-	@RequestMapping(value="/other")
-	public String other() {
-		//「/」にredirectしてる
-		return "redirect:/";
-	}
-	
-	@RequestMapping(value="/home")
-	public String home() {
-		//「/」にforwardしてる
-		return "forward:/";
-	}
-	
-//	@RequestMapping("/{month}")
-//	public ModelAndView index(@PathVariable int month, ModelAndView mav) {
-//		mav.setViewName("index");
-//		int m = Math.abs(month) % 12;
-//		m = m == 0 ? 12 : m;
-//		mav.addObject("month",m);
-//		mav.addObject("check", Math.floor(m / 3));
-//		return mav;
-//	}
 }
 
 class DataObject {
