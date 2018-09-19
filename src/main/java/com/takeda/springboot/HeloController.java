65ee0f1d6a048ae426f9918fc9b8f416d376c753
武田鴻太 <xxk0ta0605@gmail.com>
2018-09-19 22:55:36 +0900

    エンティティのCRUD

--- src/main/java/com/takeda/springboot/HeloController.java
+++ src/main/java/com/takeda/springboot/HeloController.java
@@ -1,8 +1,12 @@
 package com.takeda.springboot;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.takeda.springboot.repositories.MyDataRepository;
@@ -14,14 +18,27 @@ public class HeloController {
 	//自動的にインターフェイスに必要な処理が組み込まれたインスタンス化する
 	MyDataRepository repository;
 	
-	@RequestMapping("/")
-	public ModelAndView index(ModelAndView mav) {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index(
+			@ModelAttribute("forModel") MyData mydata,
+			ModelAndView mav) {
 		mav.setViewName("index");
 		mav.addObject("msg", "this is sample content");
 		Iterable<MyData> list = repository.findAll();  //JpaRepositoryに用意されている
-		mav.addObject("data", list);
+		mav.addObject("datalist", list);
 		return mav;
 	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@Transactional(readOnly=false)
+	public ModelAndView form(
+			@ModelAttribute("formModel") MyData mydata,
+			ModelAndView mav) {
+		repository.saveAndFlush(mydata);
+		return new ModelAndView("redirect:/");
+	}
+	
+	
 }
 
 class DataObject {
